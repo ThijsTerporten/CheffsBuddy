56445c68934b56b3ec8b4cbe761b200670da42ae
ThijsTerporten <twpterporten@gmail.com>
2021-11-15 15:35:20 +0000

    Import all dependencies for use set up connection to database

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ This document will run the app and all of its functionality
 """
 
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+
+app.config["MONOGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONOGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get(("SECRET_KEY"))
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
