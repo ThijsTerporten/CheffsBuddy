d11dd69c65b040264b9de698192a2018a01ab589
ThijsTerporten <twpterporten@gmail.com>
2021-11-15 20:05:52 +0000

    Create function for user to login

--- app.py
+++ app.py
@@ -70,6 +70,34 @@ def signup():
     return render_template("signup.html")
 
 
+@app.route("/login", methods= ["GET", "POST"])
+def login():
+    """
+    Function for loggin in using existing username
+    Check whether username exists in database
+    Ensure the hashed password matches user input
+    Ensure Username matches user input
+    """
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Incorrect username and/or password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
