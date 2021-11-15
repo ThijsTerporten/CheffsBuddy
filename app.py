af331c8c436fd88310964788d7cb807c7799aa5e
ThijsTerporten <twpterporten@gmail.com>
2021-11-15 19:53:12 +0000

    Create function for users to register to the website

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ This document will run the app and all of its functionality
 import os
 from flask import (
     Flask, flash, render_template,
-    redirect, session, url_for)
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -44,9 +44,31 @@ def get_categories():
 def signup():
     """
     Function to register to website
+    Checks wether the user exists in the database
+    If so redirects towards the signup page
+    Create object for new user that gets inserted into database
+    Create session cookie for user
     """
-    
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()}
+        )
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Succesfull")
+
     return render_template("signup.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
