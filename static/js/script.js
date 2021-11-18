13a3088347e478b9ace1a1703f2a3043c2f36158
ThijsTerporten <twpterporten@gmail.com>
2021-11-18 20:49:19 +0000

    Create variables for inst and ingredient Add function to add/remove input fields

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,13 @@ $(document).ready(function(){
     $(".tooltipped").tooltip();
   });
 
+
+// Add extra input fields in the form
+
+let ingredients = 1;
+let maxIngredients = 25;
+let directions = 1;
+let maxDirections = 40;
   $("#add-dir-btn").click(function(e) {
     e.preventDefault();
     $("#addInstruction").append(
@@ -18,8 +25,25 @@ $(document).ready(function(){
       directions++;
   });
 
+
+$("#add-ingr-btn").click(function(e) {
+  e.preventDefault();
+  $("addIngredient").append(
+    `
+    <div class="input-field">
+    <label for="ingredients">
+        Recipe Ingredients
+    </label>
+    <input type="text" name="ingredients" required>
+    <button class="btn-remove right" type="button"><i class="fas fa-trash-alt"></i></button>
+</div>`
+  );
+  ingredients++;
+});
+
+//  Remove created input fields in the form
 $("#addInstruction").on("click", ".btn-remove", function(e){
   e.preventDefault(e);
   $(this).parent("div").remove();
   directions--;
-})
+});
