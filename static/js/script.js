66b9dec37186a3bb36cce3af799d41f3f849ef72
ThijsTerporten <twpterporten@gmail.com>
2021-11-18 20:32:43 +0000

    Add function to allow user to ad/remove extra input fields for instructions in form

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,24 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $("select").formSelect();
     $(".tooltipped").tooltip();
-    $('.collapsible').collapsible();
   });
+
+  $("#add-dir-btn").click(function(e) {
+    e.preventDefault();
+    $("#addInstruction").append(
+      `
+      <div class="input-field">
+          <label for="instructions">
+              Recipe Instructions
+          </label>
+          <input type="text" name="instructions" required>
+          <button class="btn-remove right" type="button"><i class="fas fa-trash-alt"></i></button>
+      </div>`);
+      directions++;
+  });
+
+$("#addInstruction").on("click", ".btn-remove", function(e){
+  e.preventDefault(e);
+  $(this).parent("div").remove();
+  directions--;
+})
